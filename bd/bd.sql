ae8f921babec964add94e91f192e6224d55183e7
leonardo-yanez <leonardoyanez@gmail.com>
2024-05-17 18:43:51 -0400

    BD
    crea BD

---
+++ bd/bd.sql
@@ -0,0 +1,38 @@
+CREATE SCHEMA `test`;
+
+CREATE TABLE `test`.`persona` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `apellido` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan1', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan2', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan3', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan4', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan5', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan6', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan7', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan8', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan9', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan10', 'perez'); 
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan11', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan12', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan13', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan14', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan15', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan16', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan17', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan18', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan19', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan20', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan21', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan22', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan23', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan24', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan25', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan26', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan27', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan28', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan29', 'perez');
+INSERT INTO `test`.`persona` (`nombre`, `apellido`) VALUES ('juan30', 'perez');
